978567209d541cc98db20f5e9b71beebe09ccb63
git-to-know <irinaudovitsa@yandex.ru>
2020-12-14 13:11:24 +0300

    не работает скрипт1

--- source/js/mask.js
+++ source/js/mask.js
@@ -1,13 +1,9 @@
 'use strict';
 (function () {
-//   var element = document.querySelectorAll('input[name="phone-number"]');
-//   var maskOptions = {
-//     mask: '+{7}(000)000-00-00'
-//   };
-//   var mask = IMask(element, maskOptions);
-//
 
-document.querySelectorAll('.form__tel').forEach((e) => {
-  const phoneMask = IMask( e, { mask: '+{7}(000)000-00-00' })
-})
+var element = document.getElementById('tel');
+var maskOptions = {
+  mask: '+{7}(000)000-00-00'
+};
+var mask = IMask(element, maskOptions);
 })();
